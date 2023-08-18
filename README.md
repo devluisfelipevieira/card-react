5393023500b2c322c95d074ffc5a8d29fc5df9d5
Luis Felipe <luis.felipe.vieira@outlook.com>
2023-08-17 21:17:48 -0300

    adição do uso do state

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Card React
-Projeto de um card simples visando treinar o uso de ferramentas e parametros do React como Components, Fragment, props, Children e Rest Param
+Projeto de um card simples visando treinar o uso de ferramentas e parametros do React como Components, Fragment, props, Children, Rest Param, States
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,14 +1,27 @@
-import style from "./styles.module.css"
+import styles from "./styles.module.css"
 import Title from "../Title/index.jsx"
 import ProfileSection from "./ProfileSection"
 import LinkButton from "../LinkButton"
+import { useState } from "react"
 
 export default (props) => {
+  const [followText, setFollowText] = useState("Follow")
+
+  function handleClick(ev) {
+    alert("Você agora está seguindo")
+    setFollowText("Following")
+  }
   return (
-    <div className={style.profile}>
-      <img className={style.avatar} src="src/assets/perfil.png" alt="Profile Photo" />
+    <div className={styles.profile}>
+      <img className={styles.avatar} src="src/assets/perfil.png" alt="Profile Photo" />
       <Title>
         <span>{props.name}</span>
+        <button
+          className={styles.followBtn}
+          onClick={handleClick} 
+        >
+          {followText}
+        </button>
       </Title>
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
